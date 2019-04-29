848557e7abfe5721e7990819f2b638904b20ebb9
Radwa Elganady <mail2radwa@gmail.com>
2019-04-29 19:15:40 +0200

    feat: Fetching data from foursquare

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+// Import axios
+import axios from 'axios'
 import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-
+  state = {
+    locations : []
+  }
   componentDidMount() {
     this.loadMapScript();
+    this.getLocations();
   }
 
   /* initialize google Map  */
@@ -33,6 +38,33 @@ class App extends React.Component {
     index.parentNode.insertBefore(script, index);
   }
 
+  /* fetching data using axios which is a Promise based HTTP client for the browser and node.js
+   * has features -Make (XMLHttpRequests) from the browser -Make (http) requests from node.js
+   * https://github.com/axios/axios
+   *
+  */
+  getLocations = () => {
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
+    const parameters = {
+      client_id : 'TJ4M30ICUWSOT45033WN5QMJBBA0YUOD2RBTS1RW2H4LN24F',
+      client_secret: 'J30MQOQUA425OIXPCFFZDDDZLE03KADG44AZCSL0B05AWQLP',
+      query: 'hotel',
+      ll: '31.2000924,29.9187387',
+      v: '20190429',
+      limit:'15'
+    }
+    axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState({
+        locations: response.data.response.groups[0].items
+      });
+    })
+    .catch(error => {
+        // hande error of fetching data
+        alert('An error has occurred while fetching data!');
+        console.log(`Error:  ${error}`);
+      });
+    }
 
   render() {
     return (
