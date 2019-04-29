a3c254cd17c00407f1c0f60c51b9781a53138374
Radwa Elganady <mail2radwa@gmail.com>
2019-04-29 10:24:06 +0200

    chore: Render Map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  componentDidMount() {
+    this.loadMapScript();
+  }
+
+  /* initialize google Map  */
+  initMap = () => {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      // location of Alexanderia city in Egypt
+      center: {lat: 31.2000924, lng: 29.9187387},
+      zoom: 12
+    });
+  }
+  /* loading map script */
+  loadMapScript = () => {
+    window.initMap = this.initMap;
+    // first script element in the document
+    let index  = window.document.getElementsByTagName('script')[0];
+    // create script element for google map
+    let script = window.document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA2I0qEtPwrjkRVQ8BiK_FjnoWwmC4ubOY&callback=initMap';
+    script.async = true;
+    script.defer = true;
+    // handle error on loading map
+    script.onerror = function() {
+      alert('Oh no, There is an error occurred during loading map!');
+    };
+
+    index.parentNode.insertBefore(script, index);
+  }
+
+
+  render() {
+    return (
+      <main>
+        <div id="map"></div>
+      </main>
+
+    )
+  }
+
+}
 export default App;
